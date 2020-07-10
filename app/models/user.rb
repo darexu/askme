ce4952840c5db2dc6a0a46c495966f55943622ee
darexu <darexr.u@mail.ru>
2020-07-10 04:59:35 +0300

    downcase bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,7 @@ class User < ApplicationRecord
 
   has_many :questions
 
-  validates :email, :username, presence: true
-  validates :email, :username, uniqueness: true
+  validates :email, :username, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :username, format: { with: /\A[a-zA-Z0-9_]+\z/}, length: { maximum: 40 }
   validates :password, presence: true, on: :create
@@ -18,19 +17,6 @@ class User < ApplicationRecord
 
   before_validation :downcase_username, :downcase_email
   before_save :encrypt_password
-
-  def encrypt_password
-    if self.password.present?
-      # создаем т. н. "соль" - рандомная строка усложняющая задачу хакерам
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-
-      # создаем хэш пароля - длинная уникальная строка, из которой невозможно восстановить
-      # исходный пароль
-      self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS,  DIGEST.length, DIGEST)
-      )
-    end
-  end
 
   # служебный метод, преобразующий бинарную строку в 16-ричный формат, для удобства хранения
   def self.hash_to_string(password_hash)
@@ -48,12 +34,24 @@ class User < ApplicationRecord
     end
   end
 
+  def encrypt_password
+    if self.password.present?
+      # создаем т. н. "соль" - рандомная строка усложняющая задачу хакерам
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      # создаем хэш пароля - длинная уникальная строка, из которой невозможно восстановить
+      # исходный пароль
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS,  DIGEST.length, DIGEST)
+      )
+    end
+  end
+
   def downcase_username
-    self.username = self.username.downcase
+    self.username = self.username.downcase if self.username
   end
 
   def downcase_email
-    self.email = self.email.downcase
+    self.email = self.email.downcase if self.email
   end
-
 end
