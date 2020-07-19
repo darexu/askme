a194125f939e76d7c699d4b2dec1af19167f2c5f
darexu <darexr.u@mail.ru>
2020-07-19 05:17:04 +0300

    add author id to question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
 
   belongs_to :user
+  belongs_to :author, class_name: 'User'
 
   #начиная с 5 рельс связь belongs_to добавляет validates :user, presence: true автоматически
   validates :text, presence: true
