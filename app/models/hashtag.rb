55a72d056dce38d42f84acae39b7c5bd9cdd06e0
darexu <darexr.u@mail.ru>
2020-08-18 16:58:58 +0300

    Added hashtags controller

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -4,5 +4,7 @@ class Hashtag < ApplicationRecord
   has_many :hashtag_questions, dependent: :destroy
   has_many :questions, through: :hashtag_questions
 
+  scope :with_questions, -> { left_outer_joins(:hashtag_questions).where.not(hashtag_questions: { id: nil }) }
+
   validates :text, presence: true, uniqueness: true
 end
