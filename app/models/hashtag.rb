5d122435b40839332468526a796b1032b6a6df4b
darexu <darexr.u@mail.ru>
2020-08-24 05:31:21 +0300

    distinct in hashtag scope

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -4,7 +4,7 @@ class Hashtag < ApplicationRecord
   has_many :hashtag_questions, dependent: :destroy
   has_many :questions, through: :hashtag_questions
 
-  scope :with_questions, -> { left_outer_joins(:hashtag_questions).where.not(hashtag_questions: { id: nil }) }
+  scope :with_questions, -> { left_outer_joins(:hashtag_questions).where.not(hashtag_questions: { id: nil }).distinct }
 
   validates :text, presence: true, uniqueness: true
 end
